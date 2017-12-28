730d949e6a747ba126d6aabcb215c7f22a38d148
hsuanmeng <a27736994@gmail.com>
2017-12-28 17:06:37 +0800

    Update keepgoing.py

--- newfile/keepgoing.py
+++ newfile/keepgoing.py
@@ -2,4 +2,6 @@ print 'this is keepgoing.py'
 print '====end===='
 2123312
 print 'write new result from robin'
-print 'test again for master <-> 165'
+print 'test for master -> 165' # 在165
+print 'test for master <-> 165' # 跑到167去了
+print 'test for master || 165 || 167'
