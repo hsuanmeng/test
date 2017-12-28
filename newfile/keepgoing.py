f0d60997ca68d24fed1e8ab78ee7e5151e2fff06
hsuanmeng <a27736994@gmail.com>
2017-12-28 16:57:42 +0800

    Update keepgoing.py
    test node Synchronize for 165

--- newfile/keepgoing.py
+++ newfile/keepgoing.py
@@ -1,5 +1,5 @@
 print 'this is keepgoing.py'
 print '====end===='
-2123312jelkaddsad
-write new result from robin
-test for 165 and 167
+2123312
+print 'write new result from robin'
+print 'test again for master -> 165'
