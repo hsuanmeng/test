b69eabc7ac583e13b86a3996785bcd109027722d
hsuanmeng <a27736994@gmail.com>
2018-01-09 11:25:00 +0800

    Update keepgoing.py

--- newfile/keepgoing.py
+++ newfile/keepgoing.py
@@ -5,3 +5,5 @@ print 'write new result from robin'
 print 'test for master -> 165' # 在165
 print 'test for master <-> 165' # 跑到167去了
 print 'test for master || 165 || 167'
+
+print 'test for build environment'
