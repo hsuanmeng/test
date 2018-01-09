057052c73991a6848f1f8d865ee3d376dbede7d4
hsuanmeng <a27736994@gmail.com>
2018-01-09 14:43:53 +0800

    Update keepgoing.py

--- newfile/keepgoing.py
+++ newfile/keepgoing.py
@@ -6,5 +6,7 @@ print 'test for master -> 165' # 在165
 print 'test for master <-> 165' # 跑到167去了
 print 'test for master || 165 || 167'
 
-print 'test for build environment'
+print '=====test for build environment======'
 print 'test for Delete workspace before build starts'
+
+print 'test for ssh server'
