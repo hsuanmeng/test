7ac3d3570c32a6e5eea385417c4823eecc7a62d5
10509002 <a27736994@gmail.com>
2018-01-11 11:11:44 +0800

    workspace

---
+++ for_github/newfile.py
@@ -0,0 +1,9 @@
+# -*-  coding: utf-8  -*-
+
+print '===add newfile==='
+print '===test dev again==='
+
+print '===new dev11111==='
+# print '===try to merge dev to master==='
+
+print '===test for global workspace==='
